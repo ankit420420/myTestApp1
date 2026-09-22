cd48bbce9b502c0e461fef2c14d3f344d10a6de4
ankitshri774 <ankitshri774@gmail.com>
2026-09-22 12:48:43 +0530

    logs removed

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.get("/getByNameAndProfession", (req, res) => {
 
 app.get("/updated", (req, res) => {
   const { fruits } = req.query
-  console.log(fruits)
   res.json({ message: "Data Received", data: fruits })
 })
 
